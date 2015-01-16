50cd72e01645b19c95ebccbe92c6cc2088b3dca0
Tieme van Veen <tiemevanveen@gmail.com>
2015-01-16 19:02:58 +0100

    example

--- NSDateComponentsTests/ViewController.swift
+++ NSDateComponentsTests/ViewController.swift
@@ -10,16 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	@IBOutlet weak var textView: UITextView!
+
+	func startWithFirstDayOfWeek(startDay : Int?){
+
+		// January 16th 2015 10:20 AM in Amsterdam
+		var date = NSDate(timeIntervalSince1970: 1421400000)
+
+		var formatter = NSDateFormatter()
+		formatter.dateFormat = "dd-MMM"
+
+		let calendar = NSCalendar.currentCalendar()
+		if(startDay != nil && startDay > 0){
+			// Start day of the week (1 = sunday, 2 = monday, etc).
+			calendar.firstWeekday = startDay!
+		}
+
+		var output = logDay(calendar)
+
+		let units = NSCalendarUnit.YearCalendarUnit | NSCalendarUnit.MonthCalendarUnit
+			| NSCalendarUnit.WeekOfMonthCalendarUnit | NSCalendarUnit.DayCalendarUnit
+			| NSCalendarUnit.WeekdayCalendarUnit
+
+		// Loop days in January
+		for day in 1...31 {
+
+			// select day in month
+			var components = calendar.components(units, fromDate: date)
+			components.day = day
+
+			// get day and components
+			let _date = calendar.dateFromComponents(components)!
+			var _components = calendar.components(units, fromDate: _date)
+
+			// retrieve characteristics
+			let dayWeekOfMonth = _components.weekOfMonth
+			let dayOfWeek = _components.weekday
+			let month = _components.month
+
+			output += "\(formatter.stringFromDate(_date)) - is day \(dayOfWeek) of week \(dayWeekOfMonth) of month \(month) \n";
+		}
+
+		textView.text = output + "\n\n" + textView.text
+		println(output)
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	func logDay(calendar: NSCalendar) -> String {
+		switch calendar.firstWeekday {
+		case 1:
+			return "Start on day \(calendar.firstWeekday) (Sunday) \n--------\n"
+		case 2:
+			return "Start on day \(calendar.firstWeekday) (Monday) \n--------\n"
+		default:
+			return "Start on day \(calendar.firstWeekday) \n--------\n"
+		}
 	}
 
+	@IBAction func startSunday(sender: AnyObject) {
+		startWithFirstDayOfWeek(1);
+	}
 
+	@IBAction func startMonday(sender: AnyObject) {
+		startWithFirstDayOfWeek(2);
+	}
+
+	@IBAction func startDefault(sender: AnyObject) {
+		startWithFirstDayOfWeek(0);
+	}
 }
 
