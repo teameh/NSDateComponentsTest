0fcc446c6e6bd5b3d0fc180c8ad6aabb28f7acaa
Tieme van Veen <tiemevanveen@gmail.com>
2015-01-16 17:45:27 +0100

    Initial Commit

---
+++ NSDateComponentsTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSDateComponentsTests
+//
+//  Created by Tieme van Veen on 16/01/2015.
+//  Copyright (c) 2015 Tieme van Veen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
